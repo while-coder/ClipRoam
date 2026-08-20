a8d6c03b9a729865c7a0d50f7fed1504cec5d69e
linyuan.yang <linyuan.yang@centurygame.com>
2026-08-20 10:29:41 +0800

    android

---
+++ apps/app/src-tauri/gen/android/app/src/main/java/com/cliproam/mobile/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cliproam.mobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
